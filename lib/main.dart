12d9a1c79b8e09413c669b8debec3aa54d36814f
Artem <artembabichev@gmail.com>
2024-07-20 22:33:00 +0300

    init commit env

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mindflasher/screens/list/flashcard_list_screen.dart';
 import 'package:provider/provider.dart';
+import 'env_config.dart';
 import 'providers/flashcard_provider.dart';
 
 
@@ -21,6 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: EnvConfig.showDebugUpRightBanner,
       title: 'Flashcard App',
       home: FlashcardListScreen(),
 
